3f39de274c85049c786473153a602ec7203cc530
tavu <tavu@linux-t06i.site>
2013-07-17 15:26:54 +0300

    initial commit

---
+++ src/autobahn/android/mainActivity.java
@@ -0,0 +1,16 @@
+package autobahn.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.autobahn.R;
+
+public class mainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

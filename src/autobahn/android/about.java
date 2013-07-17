41b569cb5117fa1e2780eb95cce6e9930873629b
tavu <tavu@linux-t06i.site>
2013-07-17 15:51:07 +0300

    first activities

---
+++ src/autobahn/android/about.java
@@ -0,0 +1,17 @@
+package autobahn.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tavu
+ * Date: 7/17/13
+ * Time: 3:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class about extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ src/autobahn/android/trackCircuit.java
@@ -0,0 +1,17 @@
+package autobahn.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tavu
+ * Date: 7/17/13
+ * Time: 3:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class trackCircuit extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

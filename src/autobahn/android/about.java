58dc18fa1408b02ecddd448f8575c6c0dc917340
Nikos Stathopoulos <stathopou@ceid.upatras.gr>
2013-07-18 15:57:07 +0300

    Created About Activity

--- src/autobahn/android/about.java
+++ src/autobahn/android/about.java
@@ -1,7 +1,12 @@
 package autobahn.android;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import com.example.autobahn.R;
 
 /**
@@ -12,9 +17,99 @@ import com.example.autobahn.R;
  * To change this template use File | Settings | File Templates.
  */
 public class about extends Activity {
+
+    public final static Uri GEANT_URL = Uri.parse("http://www.geant.net");
+    public final static Uri DANTE_URL = Uri.parse("http://www.dante.net");
+    public final static Uri EC_URL = Uri.parse("http://ec.europa.eu");
+    public final static Uri BOD_URL = Uri.parse("bod.geant.net");
+    public final static Uri AUTOBAHN_URL = Uri.parse("autobahn.geant.net");
+    public final static Uri ISSUES_URL = Uri.parse("https://issues.geant.net/jira/browse/MDSD");
+
+    Intent launchBrowser;
+    TextView link;
+    ImageButton button;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.about);
+
+        link = (TextView) findViewById(R.id.link_1);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,GEANT_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        button = (ImageButton) findViewById(R.id.geantLogo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,GEANT_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        button = (ImageButton) findViewById(R.id.danteLogo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser =  new Intent(Intent.ACTION_VIEW,DANTE_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        button = (ImageButton) findViewById(R.id.euLogo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,EC_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        link = (TextView) findViewById(R.id.ecLink);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,EC_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        link = (TextView) findViewById(R.id.link_2);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,BOD_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+        link = (TextView) findViewById(R.id.link_3);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,AUTOBAHN_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+        link = (TextView) findViewById(R.id.link_4);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchBrowser = new Intent(Intent.ACTION_VIEW,ISSUES_URL);
+                startActivity(launchBrowser);
+            }
+        });
+
+
+
     }
 }

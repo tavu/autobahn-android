cdf05c239869875ae818fde6ca7ff16607b88ac9
Nikos Stathopoulos <stathopou@ceid.upatras.gr>
2013-10-09 16:50:40 +0300

    Removed Commented code snippets

--- src/autobahn/android/AutobahnClient.java
+++ src/autobahn/android/AutobahnClient.java
@@ -280,17 +280,4 @@ public class AutobahnClient {
     }
     */
 
-
-    private String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
-        InputStream in = entity.getContent();
-        StringBuffer out = new StringBuffer();
-        int n = 1;
-        while (n > 0) {
-            byte[] b = new byte[4096];
-            n = in.read(b);
-            if (n > 0) out.append(new String(b, 0, n));
-        }
-        return out.toString();
-    }
-
 }

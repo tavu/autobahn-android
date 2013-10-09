ee249b52660888e57c4077177c0af74fff3d4890
tavu <tavu@linux-t06i.site>
2013-10-09 15:12:33 +0300

    RequestActivity2

---
+++ src/autobahn/android/RequestActivity2.java
@@ -0,0 +1,23 @@
+package autobahn.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.autobahn.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tavu
+ * Date: 9/24/13
+ * Time: 2:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestActivity2  extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.request_reservation_activity2);
+    }
+
+
+}

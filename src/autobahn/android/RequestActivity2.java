57fbfbca8a278c4b5fb53d6c1152d645b0104614
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2013-10-11 18:06:05 +0300

    datePicker done

--- src/autobahn/android/RequestActivity2.java
+++ src/autobahn/android/RequestActivity2.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+
 import com.example.autobahn.R;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,48 +25,52 @@ import java.util.Calendar;
  * Time: 2:26 PM
  * To change this template use File | Settings | File Templates.
  */
-public class RequestActivity2  extends Activity implements  DatePickerDialog.OnDateSetListener {
+public class RequestActivity2 extends Activity implements DatePickerDialog.OnDateSetListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.request_reservation_activity2);
+		setContentView(R.layout.request_reservation_activity2);
 
-        EditText txt = (EditText) this.findViewById(R.id.startVlan);
-        txt.setInputType(InputType.TYPE_NULL);
+		EditText editText = (EditText) findViewById(R.id.startDate);
+		editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					showDatePicker();
+				}
+			}
+		});
+	}
 
-        txt = (EditText) this.findViewById(R.id.endVlan);
-        txt.setInputType(InputType.TYPE_NULL);
+	public void showNumberPicker(View v) {
+		// TODO: change min API level to 11
+		NumberPicker numb = new NumberPicker(this);
 
-        txt = (EditText) this.findViewById(R.id.startTime);
-        txt.setInputType(InputType.TYPE_NULL);
-        txt = (EditText) this.findViewById(R.id.startDate);
-        txt.setInputType(InputType.TYPE_NULL);
-        txt = (EditText) this.findViewById(R.id.endTime);
-        txt.setInputType(InputType.TYPE_NULL);
-        txt = (EditText) this.findViewById(R.id.endDate);
-        txt.setInputType(InputType.TYPE_NULL);
-    }
+		Dialog dialog = new Dialog(this);
+		ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-    public void showNumberPicker(View v) {
-        NumberPicker numb=new NumberPicker(this);
+		dialog.addContentView(numb, lp);
+		dialog.show();
 
-        Dialog dialog=new Dialog(this );
-        ViewGroup.LayoutParams lp =new  ViewGroup.LayoutParams( ViewGroup.LayoutParams.WRAP_CONTENT,  ViewGroup.LayoutParams.WRAP_CONTENT);
+	}
 
-        dialog.addContentView(numb,lp);
-        dialog.show();
+	public void showDatePicker() {
+		// TODO: change min API level to 11
+		DatePickerDialog dialog = new DatePickerDialog(this, this, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+		dialog.show();
+	}
 
-    }
-
-    public void showDatePicker(View v) {
-        DatePickerDialog dialog=new DatePickerDialog(this , null,1,1,1);
-        dialog.show();
-        DatePicker d=dialog.getDatePicker();
-    }
-
-    public void	 onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-
-    }
+	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+		EditText dateDisplay = (EditText) findViewById(R.id.startDate);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date date = null;
+		try {
+			date = sdf.parse(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+		} catch (ParseException e) {
+		}
+		DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(this);
+		dateDisplay.setText(dateFormat.format(date));
+	}
 }

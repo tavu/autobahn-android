161a5b0fe4d35c06670413e27effe840521fede2
Mike Drakoulelis <mikedrakoulelis@yahoo.gr>
2013-10-16 15:49:01 +0300

    Set theme and text colors to application

--- src/autobahn/android/IdmsActivity.java
+++ src/autobahn/android/IdmsActivity.java
@@ -7,7 +7,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.autobahn.R;
 
 import java.util.List;
@@ -15,80 +21,80 @@ import java.util.List;
 
 public class IdmsActivity extends Activity {
 
-    List<String> domains;
-    ListView domainList;
-    ArrayAdapter<String> adapter;
-    AutobahnClientException exception=null;
+	List<String> domains;
+	ListView domainList;
+	ArrayAdapter<String> adapter;
+	AutobahnClientException exception = null;
 
-    private void showData() {
-        domains = AutobahnClient.getInstance().getIdms();
+	private void showData() {
+		domains = AutobahnClient.getInstance().getIdms();
 
-        if(exception != null) {
-            Toast toast  = Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG);
-            toast.show();
-        }
-        if (domains.isEmpty()) {
-            setContentView(R.layout.no_domains);
-            Button menuButton = (Button) findViewById(R.id.menuButton);
-            menuButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent menu = new Intent();
-                    menu.setClass(getApplicationContext(), MainMenu.class);
-                    startActivity(menu);
-                    finish();
-                }
-            });
-        } else {
-            setContentView(R.layout.idm_selection_activity);
-            domainList = (ListView) findViewById(R.id.listView);
-            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, domains);
+		if (exception != null) {
+			Toast toast = Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG);
+			toast.show();
+		}
+		if (domains.isEmpty()) {
+			setContentView(R.layout.no_domains);
+			Button menuButton = (Button) findViewById(R.id.menuButton);
+			menuButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					Intent menu = new Intent();
+					menu.setClass(getApplicationContext(), MainMenu.class);
+					startActivity(menu);
+					finish();
+				}
+			});
+		} else {
+			setContentView(R.layout.idm_selection_activity);
+			domainList = (ListView) findViewById(R.id.listView);
+			adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, domains);
 
-            domainList.setAdapter(adapter);
-            domainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    TextView item = (TextView)view;
-                    item.setTextColor(R.color.custom_text_color_autobahn_android);
-                    item.setBackgroundColor(Color.WHITE);
-                    String domain  = item.getText().toString();
-                    Intent domainActivity = new Intent();
-                    domainActivity.setClass(getApplicationContext(),TrackCircuitActivity.class);
-                    domainActivity.putExtra("DOMAIN_NAME",domain);
-                    startActivity(domainActivity);
-                }
-            });
+			domainList.setAdapter(adapter);
+			domainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+					TextView item = (TextView) view;
+					item.setTextColor(R.color.textColor);
+					item.setBackgroundColor(Color.WHITE);
+					String domain = item.getText().toString();
+					Intent domainActivity = new Intent();
+					domainActivity.setClass(getApplicationContext(), TrackCircuitActivity.class);
+					domainActivity.putExtra("DOMAIN_NAME", domain);
+					startActivity(domainActivity);
+				}
+			});
 
-        }
-    }
+		}
+	}
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        AsyncTask<Void, Void, Void> async = new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... type) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AsyncTask<Void, Void, Void> async = new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... type) {
 
-                try {
-                    AutobahnClient.getInstance().fetchIdms();
-                } catch (AutobahnClientException e) {
-                    exception=e;
-                }
-                return null;
-            }
+				try {
+					AutobahnClient.getInstance().fetchIdms();
+				} catch (AutobahnClientException e) {
+					exception = e;
+				}
+				return null;
+			}
 
-            @Override
-            protected void onProgressUpdate(Void... progress) {
-                super.onProgressUpdate(progress);
-            }
+			@Override
+			protected void onProgressUpdate(Void... progress) {
+				super.onProgressUpdate(progress);
+			}
 
-            @Override
-            protected void onPostExecute(Void result) {
-                Log.d("WARN","Done Fetching Domains!");
-                showData();
-            }
-        };
-        async.execute();
+			@Override
+			protected void onPostExecute(Void result) {
+				Log.d("WARN", "Done Fetching Domains!");
+				showData();
+			}
+		};
+		async.execute();
 
 
-    }
+	}
 }
